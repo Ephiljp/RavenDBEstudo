5e333736b8754c4fa73657c357684fb8a24684f5
Felipe Hideo Yamaguchi <felipehideo@hotmail.com>
2017-03-20 14:08:40 -0300

    Criado o banco de dados e funçoes de gravar, consultar e remover

--- RavenDBTestedeBancoDeDados/MainWindow.xaml.cs
+++ RavenDBTestedeBancoDeDados/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,67 @@ namespace RavenDBTestedeBancoDeDados
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string idDoClienteSalvo { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            var cliente = new Cliente
+            {
+                Nome = txtCliente.Text,
+                CPF = "3251651",
+                Email = "asdasdas@asdas",
+                Endereco = new Endereco
+                              
+                {
+                    Cidade = "Sao Paulo",
+                    Complemento = "Casa Verde",
+                    Estado = "SP",
+                    Logradouro = "Rua 01",
+                    Numero = 123
+                }
+
+            };
+
+            var repositorio = new RepositorioGenerico();
+
+            var idCliente =  repositorio.Cadastrar(cliente);
+
+            idDoClienteSalvo = idCliente;
+
+            MessageBox.Show($"Cadastrado com sucesso ({ idCliente})");
+
+          
+    }
+
+        private void btnConsulta_Click(object sender, RoutedEventArgs e)
+        {
+            var repositorio = new RepositorioGenerico();
+
+            var cliente = repositorio.Consulte(idDoClienteSalvo);
+
+            MessageBox.Show($"Consultado o cliente {cliente.Nome} ");
+
+        }
+
+        private void btnRemover_Click(object sender, RoutedEventArgs e)
+        {
+            if (idDoClienteSalvo == null)
+            {
+                MessageBox.Show("Não há o que remover");
+                return;
+            }
+            var repositorio = new RepositorioGenerico();
+
+            repositorio.Remover(idDoClienteSalvo);
+
+            MessageBox.Show("Removido com sucesso");
+            return;
         }
     }
 }
---
+++ Repositorio/RepositorioGenerico.cs
@@ -0,0 +1,68 @@
+﻿using Model;
+using Raven.Client;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class RepositorioGenerico
+    {
+        public DocumentStore store { get; set; }
+
+        public RepositorioGenerico()
+
+        {
+            store = new DocumentStore
+
+            {
+                Url = "http://localhost:8080",
+
+                DefaultDatabase = "RavenDBTestedeBancoDeDados"
+            };
+
+            store.Initialize();
+
+
+
+        }
+
+        public string Cadastrar(Cliente cliente)
+        {
+
+            using (IDocumentSession session = store.OpenSession())
+            {
+                session.Store(cliente);
+                session.SaveChanges();
+
+            }
+            return cliente.Id;
+
+        }
+
+        public Cliente Consulte(string idDoClienteSalvo)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                return session.Load<Cliente>(idDoClienteSalvo);
+
+
+            }
+
+        }
+
+        public void Remover(string idDoClienteSalvo)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                session.Delete(idDoClienteSalvo);
+                session.SaveChanges();
+
+            }
+
+        }
+    }
+}
